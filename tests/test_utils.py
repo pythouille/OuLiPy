c0f360413be1a04fbb18264e62ff59ad2cfe5259
Louis Couturier <couturier.louis97@gmail.com>
2024-02-17 00:00:16 +0100

    add empty string case in all tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,6 +6,7 @@ from src.utils import *
 
 class TestIsPalindrom(unittest.TestCase):
     def test_remove_non_word(self):
+        self.assertEqual(remove_non_word(""), "")
         self.assertEqual(remove_non_word("fenouil"), "fenouil")
         self.assertEqual(remove_non_word("À-côtés"), "Àcôtés") # Accents
         self.assertEqual(remove_non_word("!:;,Ahah,;:!"), "Ahah") # Punctuation
@@ -13,11 +14,13 @@ class TestIsPalindrom(unittest.TestCase):
         self.assertEqual(remove_non_word("Parfois, ou peut-être pas (!) ?"), "Parfoisoupeutêtrepas") # Both
 
     def test_remove_accent(self):
+        self.assertEqual(remove_accent(""), "")
         self.assertEqual(remove_accent("fenouil"), "fenouil")
         self.assertEqual(remove_accent("À-côtés"), "A-cotes")
         self.assertEqual(remove_accent("Deçà delà"), "Deca dela")
 
     def test_is_palindrom(self):
+        self.assertTrue(is_palindrom(""))
         self.assertFalse(is_palindrom("fenouil"))
         self.assertTrue(is_palindrom("kayak"))
         self.assertTrue(is_palindrom("XxXx")) # Not case sensitive
@@ -27,12 +30,14 @@ class TestIsPalindrom(unittest.TestCase):
         self.assertFalse(is_palindrom("Xsope este ici et se repose."))
 
     def test_lipogram(self):
+        self.assertTrue(is_lipogram("", "e"))
         self.assertFalse(is_lipogram("fenouil", "e"))
         self.assertTrue(is_lipogram("kayak", "e"))
         self.assertFalse(is_lipogram("E", "e")) # Case insensitive
         self.assertFalse(is_lipogram("ê", "e")) # Not sensitive to accents
 
     def test_prisoner(self):
+        self.assertTrue(check_prisoner(""))
         self.assertFalse(check_prisoner("fenouil"))
         self.assertFalse(check_prisoner("fenouil", allow_accent=False))
         self.assertFalse(check_prisoner("Xxxx."))
@@ -44,6 +49,7 @@ class TestIsPalindrom(unittest.TestCase):
         self.assertFalse(check_prisoner("sans un son, sans un sou", allow_accent=False)) # ','
 
     def test_abecedaire(self):
+        self.assertFalse(check_abecedaire(""))
         self.assertFalse(check_abecedaire("fenouil"))
         self.assertTrue(check_abecedaire(
             "Axxx. Bxxx, cxéxxx dxx exxxx, fxxx gxxxxxx. "\
