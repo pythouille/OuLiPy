7fa88087eb9c3db2dcde1834f55be6874bc321b8
Louis Couturier <couturier.louis97@gmail.com>
2024-02-16 21:33:22 +0100

    add and test a function to remove punctuation and white space from strings

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,21 @@
 """
 This module contains functions to check properties in strings.
 """
+import string
 
 
+
+def remove_non_word(s: str) -> str:
+    """
+    Return a copy of given string without
+    its punctuation and white space (which
+    are non-word character).
+    """
+    s_copy = s
+    for c in (string.punctuation + string.whitespace):
+        if c in s_copy:
+            s_copy = s_copy.replace(c, '')
+    return s_copy
 
 def is_palindrom(s: str) -> bool:
     """
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,9 +5,21 @@ from src.utils import *
 
 
 class TestIsPalindrom(unittest.TestCase):
+    def test_remove(self):
+        self.assertEqual(remove_non_word("fenouil"), "fenouil")
+        self.assertEqual(remove_non_word("À-côtés"), "Àcôtés") # Accents
+        self.assertEqual(remove_non_word("!:;,Ahah,;:!"), "Ahah") # Punctuation
+        self.assertEqual(remove_non_word(" Ah  ah\t\tah\nah  "), "Ahahahah") # White space
+        self.assertEqual(remove_non_word("Parfois, ou peut-être pas (!) ?"), "Parfoisoupeutêtrepas") # Both
+
     def test_is_palindrom(self):
-        self.assertTrue(is_palindrom("kayak"))
         self.assertFalse(is_palindrom("fenouil"))
+        self.assertTrue(is_palindrom("kayak"))
+        #TODO
+        #self.assertTrue(is_palindrom("XxXx")) # Not case sensitive
+        #self.assertTrue(is_palindrom("x\nx x\tx   ")) # Not sensitive to white space
+        #self.assertTrue(is_palindrom("x..x!x?!!x")) # Not sensitive to punctuation
+        #self.assertTrue(is_palindrom("Ésope reste ici et se repose."))
 
 
 
